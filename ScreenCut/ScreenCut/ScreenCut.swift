de845cb7fad517b640fd68185aac87321980505e
helinyu <helinyu@users.noreply.github.com>
2024-10-30 22:29:15 +0800

    添加了调用AI的功能

--- ScreenCut/ScreenCut/ScreenCut.swift
+++ ScreenCut/ScreenCut/ScreenCut.swift
@@ -145,6 +145,42 @@ class ScreenCut {
         let nsImage = NSImage(cgImage: cgImage, size: NSSize(width: cgImage.width, height: cgImage.height))
         return nsImage
     }
+    
+    func transforRequest(_ text: String, then: @escaping (String?, Bool) -> Void)  {
+        let urlString = "http://127.0.0.1:5000/translate" // 应该是本地没有解析localhost，要使用127.0.0.1
+        guard let url = URL(string: urlString) else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let parameters: [String: Any] = ["text": text] // 替换为你的参数
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: [])
+        } catch {
+            print("Error encoding parameters: \(error)")
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String: Any] {
+                    print("Response Dictionary: \(json)")
+                    let translatedText:String = json["translated_text"] as! String
+                    then(translatedText, true)
+
+                } else {
+                    print("Failed to convert data to dictionary")
+                    then(nil, false)
+                }
+            } catch {
+                print("Error parsing JSON: \(error)")
+                then(nil, false)
+            }
+        }
+
+        task.resume()
+    }
 
 
 }
